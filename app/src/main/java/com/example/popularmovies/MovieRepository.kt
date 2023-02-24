5317c48cd4359faa3b108caf393cb2247d8784d3
prasad-333 <37048768+prasad-333@users.noreply.github.com>
2023-02-24 18:46:12 +0530

    technical review chapter 14 with 14.01 as codebase in this commit

---
+++ app/src/main/java/com/example/popularmovies/MovieRepository.kt
@@ -0,0 +1,29 @@
+package com.example.popularmovies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.popularmovies.api.MovieService
+import com.example.popularmovies.model.Movie
+
+class MovieRepository(private val movieService: MovieService) {
+
+    private val apiKey = "4b6a464636483bd0c7d5451186b74c0e"
+
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    private val errorLiveData = MutableLiveData<String>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+    val error: LiveData<String>
+        get() = errorLiveData
+
+
+    suspend fun fetchMovies() {
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (exception: Exception) {
+            errorLiveData.postValue("An error occurred: ${exception.message}")
+        }
+    }
+
+}
